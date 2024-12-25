3ae65baeb1be029937bd79063cf461f3794b72f0
ajkkln <alina.meow.228@mail.ru>
2024-12-25 10:44:53 +0400

    fix

--- lattegallery/accounts/routers.py
+++ lattegallery/accounts/routers.py
@@ -1,10 +1,17 @@
 from fastapi import Depends, APIRouter, HTTPException, status
-
+from fastapi import FastAPI
 from fastapi.params import Depends
 from fastapi.security import HTTPAuthorizationCredentials
 from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import PositiveInt
-
+import jwt
+import datetime
+from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from typing import Optional
 
 from lattegallery.accounts.schemas import (
     AccountCreateSchema,
@@ -166,5 +173,122 @@ async def create_account(
 async def protected_route(current_user: Account = Depends(get_current_user)):
     return {"message": f"Hello, {current_user.name}!"}
 
+SECRET_KEY = "12345" 
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
+class User(BaseModel):
+    username: str
+
+class UserInDB(User):
+    hashed_password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str
+
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$KIX/6Z0x3T3zG1JZz5g4UuXc9h5sB8U5eB4Q0FQkK7X5A6v6S0x6C",  # password
+        "disabled": False,
+    }
+}
+
+
+def verify_password(plain_password, hashed_password):
+    from passlib.context import CryptContext
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@app.post("/token", response_model=Token)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = get_user(fake_users_db, form_data.username)
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+
+    user = get_user(fake_users_db, token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@app.get("/users/me", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
+SECRET_KEY = '12345'  
+def generate_jwt_token(username):
+    
+    expiration = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    
+ 
+    payload = {
+        'username': username,
+        'exp': expiration
+    }
+  
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return token
+def decode_jwt_token(token):
+    try:
+      
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        return payload
+    except jwt.ExpiredSignatureError:
+        return 'Token has expired'
+    except jwt.InvalidTokenError:
+        return 'Invalid token'
